ab499c75bf8147c4a274a2f33b07a8c4a58a0b6c
Colton Broughton <cole.broughton@gmail.com>
2025-08-21 20:52:25 -0600

    comments, logic simplification, new func

--- DiscordBot.py
+++ DiscordBot.py
@@ -12,14 +12,18 @@ desc:
 import discord
 import random
 from discord.ext import commands
+# locallt define config file
 import config
 
 
 class DiscordBot(commands.Bot):
+    
     def __init__(self, token, logger, channels: dict):
+        # applying the bots default intents
         intents = discord.Intents.default()
+        # bot requires the message_content intent from the dev portal for discord apps.
         intents.message_content = True  # Required for reading messages
-
+        # assigning the command prefix, and the bots required intents
         super().__init__(command_prefix="!", intents=intents)
         
         self.logger = logger
@@ -30,11 +34,15 @@ class DiscordBot(commands.Bot):
     
     # function to handle message creation/send events.     
     async def on_message(self, message: discord.Message):
+        # if the the message is from the bot, ignore it.
         if message.author != self.user:
+            # save message to the log
             self.logger.info(f"Message from {message.author}: {message.content}")
+            # check to see if a random message is being sent
             await self.random_message()
+            
             await self.handle_message(message)
-
+            
         if message.content.startswith('$hello'):
             await message.channel.send('Hello World!')
     
@@ -46,9 +54,8 @@ class DiscordBot(commands.Bot):
             self.logger.info(f"sent message: {msg}")
             await self.get_channel(self.__channels["Botting"]).send(msg) # type: ignore
 
-    
-    # function to handle sending messages
-    async def handle_message(self, message):
+
+    async def message_check(self, message):
         banned_words = set(config.BAN_WORDS)
         unique_words = set(message.content.lower().split(" "))
         
@@ -60,7 +67,16 @@ class DiscordBot(commands.Bot):
                 self.__beetle_juice_counter += 1
 
         if banned_words & unique_words:
-            await self.get_channel(self.__channels["Botting"]).send(f"You're going to jail {message.author}") # type: ignore
+            response = f"You're going to jail {message.author}"
+            await self.handle_message(message, response)
+            
+    async def troll_user(self, message):
+         if message.author.id == config.user_ids[0]:
+            await self.handle_message(message, )
+            
+    # function to handle sending messages
+    async def handle_message(self, message, response=None):
+       await self.get_channel(self.__channels["Botting"]).send(response) # type: ignore
     
     
     # Running the bot. 
