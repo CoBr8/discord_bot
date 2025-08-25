fc5c10d7b9ee4c4f2836780645e5b1d6834af67c
Colton Broughton <cole.broughton@gmail.com>
2025-08-25 11:39:17 -0600

    timestamped log name.

--- main.py
+++ main.py
@@ -6,23 +6,24 @@ auth:   CoBr8
 desc:
     Importing class modules and config information to generate main script body.
 """
-
 from logger import Logger # logging class to handle server side logging
 from DiscordBot import DiscordBot # DiscordBot class to handle Guild interactions
 from pathlib import Path # pathlib > sys
 from os import getpid
 import logging # python logging module
 import config # configuration file, local environment defined
+from datetime import datetime
+
+LOG_NAME = "discord_bot_" +  datetime.now().strftime("%Y-%m-%dT%H:%M") + ".log"
 
 def main():       
     # define path to where we want the log file to exist
     LOG_PTH = Path.cwd() / Path("LOGS/") / (__name__  + ".log")
-    
     pid_file = LOG_PTH.parent.parent / Path("kill_bot.sh")
     pid_file.write_text(f"#!/bin/zsh\nkill {getpid()}")
     
     # generating out log object using custom logger class
-    logs = Logger(__name__, LOG_PTH, logging.INFO)
+    logs = Logger(LOG_NAME, LOG_PTH, logging.INFO)
     
     # inform log that we are starting the bot
     logs.info("Starting Discord Bot")
